3ba85fab825d1218b7470c87297bc879c9e948c8
Nicholas Ali <al.nicholas.ali@impatech.edu.br>
2026-08-10 17:10:37 -0300

    j

---
+++ Funcaoqueusamytools.py
@@ -0,0 +1,9 @@
+from mytools import MYTOOLS
+print("digite com quantas casas você quer o arredondamento de pi:")
+MYTOOLS.pi_real()
+print("digite com quantas casas você quer o arredondamento de e:")
+MYTOOLS.e_real()
+
+print(f"Pi = {MYTOOLS.E_INT}")
+print(f"Euler = {MYTOOLS.PI_INT}")
+
